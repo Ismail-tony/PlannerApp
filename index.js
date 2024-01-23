20eb07bca368acfd370eaeee6c01598bb89b0ba7
Ismail-tony <ibabalola21@outlook.com>
2024-01-23 15:04:48 +0000

    Creating the timeblock in the html file wasn't working as i hope it would

--- index.js
+++ index.js
@@ -5,14 +5,15 @@
 // Display the current day at the top of the calendar when a user opens the planner.
 const currentDay = dayjs();
 $('#currentDay').text(currentDay.format('dddd, D MMM YYYY'));
-console.log(currentDay);
 
 // Present timeblocks for standard business hours when the user scrolls down.
-
+for (let hour = 9; hour <= 17; hour++) {
+    const time = dayjs('${hour}').format('h A');
+}
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 
 // Allow a user to enter an event when they click a timeblock.
-
+$('form').on(`submit`)
 // Save the event in local storage when the save button is clicked in that timeblock.
 
 // Persist events between refreshes of a page.
