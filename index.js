8a82eb6b319f8e5530c30a2740769ad4fb0cc847
Ismail-tony <ibabalola21@outlook.com>
2024-01-24 11:44:21 +0000

    testing array method for timeblock

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ $('#currentDay').text(currentDay.format('dddd, D MMM YYYY'));
 
 // Present timeblocks for standard business hours when the user scrolls down.
 for (let hour = 9; hour <= 17; hour++) {
-    const time = dayjs('${hour}').format('h A');
+    const time = hour;
+    console.log(time);   
 }
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-
+function hour() {
+    const currentTime = dayjs().hour();
+    if (hour < currentTime) {
+        
+    }
+}
 // Allow a user to enter an event when they click a timeblock.
 $('form').on(`submit`)
 // Save the event in local storage when the save button is clicked in that timeblock.
