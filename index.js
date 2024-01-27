660927954caef3fd8377f62b9bb26d9f52caf0d8
Ismail-tony <ibabalola21@outlook.com>
2024-01-27 23:05:50 +0000

    append the form

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 // This Challenge will require you to leverage all the skills you've learned so far, requiring you to think about DOM manipulation, 
 // advanced JavaScript, integration of third-party APIs, and more. 
 // This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+const containerEl = $('.container');
+const currentDayEl = $('#currentDay');
 
 // Display the current day at the top of the calendar when a user opens the planner.
-const currentDay = dayjs();
-$('#currentDay').text(currentDay.format('dddd, D MMM YYYY'));
+const currentDay = dayjs().format('dddd, Do MMM YYYY');
+currentDayEl.text(currentDay);
 
 // Present timeblocks for standard business hours when the user scrolls down.
 for (let hour = 9; hour <= 17; hour++) {
-    const time = hour;
-    console.log(time);   
+    const am_pm = dayjs(`2024 ${hour}`).format('h A') // formated the array to the standard clock widely used.
+    const timeblock = 
+    `
+    <form>
+        <label class=col-2 for=entry>${am_pm}</label>
+        <textarea class=col-8 name=description></textarea>
+        <button type=submit class=saveBtn col-2><i class="bi bi-save"></i></button>
+    </form>
+`;
+containerEl.append(timeblock)
 }
+
+
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-function hour() {
-    const currentTime = dayjs().hour();
-    if (hour < currentTime) {
-        
-    }
-}
+
 // Allow a user to enter an event when they click a timeblock.
 $('form').on(`submit`)
 // Save the event in local storage when the save button is clicked in that timeblock.
