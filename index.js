5feb110f5c5f700c049babaa5de402c01f1cd7de
Ismail-tony <ibabalola21@outlook.com>
2024-01-31 15:33:56 +0000

    added the timeblock in the html for easier understanding as well as setting the colorcode, local storage and persist the events

--- index.js
+++ index.js
@@ -1,75 +1,50 @@
-//Jquerry Element
-const containerEl = $('.container');
-const currentDayEl = $('#currentDay');
-
-let time = [09, 10, 11, 12, 13, 14, 15, 16, 17];
-const currenthour = dayjs().hour()
-// Display the current day at the top of the calendar when a user opens the planner.
-const currentDay = dayjs().format('dddd, Do MMM YYYY');
-currentDayEl.text(currentDay);
-
-// Present timeblocks for standard business hours when the user scrolls down.
-function getTimeBlock() {
-    for (let i = 0; i<=time.length -1; i++) { //reason for the -1 is because after the loop, an undefined object was present 
-        /* const am_pm = dayjs(`2024 ${hour}`).format('h A') 
-        formated the array to the standard clock widely used 
-        but i couldn't get it to work with color changing. */
-        const hours = time[i];
-        const timeblock = 
-        `
-        <form class='time-block row'>
-            <label class='hour col-2' for='hour'>${hours}:00</label>
-            <textarea class='description col-8' name='description'></textarea>
-            <button type='submit' class='saveBtn col-2'><i class="fa-regular fa-floppy-disk"></i></button>
-        </form>
-        `;
-    containerEl.append(timeblock)
-    } 
-};
-// for (let i = 0; i<=time.length -1; i++) { //reason for the -1 is because after the loop, an undefined object was present 
-//     /* const am_pm = dayjs(`2024 ${hour}`).format('h A') 
-//     formated the array to the standard clock widely used 
-//     but i couldn't get it to work with color changing. */
-//     const hours = time[i];
-//     const timeblock = 
-//     `
-//     <form class='time-block row'>
-//         <label class='hour col-2' for='hour'>${hours}:00</label>
-//         <textarea class='description col-8' name='description'></textarea>
-//         <button type='submit' class='saveBtn col-2'><i class="fa-regular fa-floppy-disk"></i></button>
-//     </form>
-//     `;
-// containerEl.append(timeblock)
-// }; 
+//jQuery
+$(document).ready(function () { //when the document is ready
+    // let time = [9, 10, 11, 12, 13, 14, 15, 16, 17]; // times in 24 hours format
+    const containerEl = $('.container');
+    const currentDayEl = $('#currentDay');
 
 
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+    // Display the current day at the top of the calendar when a user opens the planner.
+    const currentDay = dayjs().format('dddd, Do MMM YYYY');
+    currentDayEl.text(currentDay);
+    $('.saveBtn').on('click', function () {
+        const value = $(this).siblings('.description').val();
+        const time = $(this).parent().attr('id');
+        // SETTING UP LOCAL STORAGE SET ITEMS
 
-function changeColor() {
-time.forEach(hours => {
-    if (time < currenthour) { 
-        return "past"; 
-    } else if (time === currenthour) { 
-        return "present";
-    } else {
-        return "future";
+        localStorage.setItem(time, value);
+    });
+
+    // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+    function getColorCode() {
+        const currenthour = dayjs().hour()
+        $(`.timeblock`).each(function () {
+            const blockhour = parseInt($(this).attr('id').split('-')[1]);
+            if (blockhour < currenthour) {
+                $(this).addClass('past')
+            } else if (blockhour === currenthour) {
+                $(this).removeClass('past')
+                $(this).addClass('present')
+            } else {
+                $(this).removeClass('past')
+                $(this).removeClass('present')
+                $(this).addClass('future')
+            }
+        })
     }
+    getColorCode();
+    setInterval(getColorCode, 15000);
+    // Persist events between refreshes of a page.
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
 });
 
-
-
-    $(".timeblock").each(function(i, timeblock) { 
-        if (time < currenthour) { 
-            return "past"; 
-        } else if (time === currenthour) { 
-            return "present";
-        } else {
-            return "future";
-        }
-    })
-}
-// Allow a user to enter an event when they click a timeblock.
-$('form').on(`submit`)
-// Save the event in local storage when the save button is clicked in that timeblock.
-
-// Persist events between refreshes of a page.
